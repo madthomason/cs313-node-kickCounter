eb8d51c31f328c08f31b4c2989402c544a45be6c
madthomason <tho16031@byui.edu>
2019-03-16 17:01:14 -0600

    login works and sends to main page

--- db.sql
+++ db.sql
@@ -3,9 +3,8 @@ CREATE TABLE mother (
  username  varchar(60),
  password  varchar(255));
 
--- INSERT INTO mother (name, password)
---  VALUES 	('Madeline', 'tho16031@byui.edu'),
---  			('Sue', 'sue@byui.edu');
+
+ ALTER TABLE mother ADD CONSTRAINT uniq_user UNIQUE (username);
 
    CREATE TABLE kick_session (
    id           serial PRIMARY KEY,
@@ -13,15 +12,15 @@ CREATE TABLE mother (
    end_time     TIMESTAMP,
    mother_id  	int REFERENCES mother (id));
 
-   INSERT INTO kick_session (start_time, end_time, mother_id)
-    VALUES ('2004-10-19 10:23:54', '2004-10-19 10:23:54', (SELECT id FROM person WHERE name = 'madeline')),
-    ('2004-10-19 10:23:54', '2004-10-19 10:23:54', (SELECT id FROM person WHERE name = 'Sue')),
-    ('2004-10-19 10:23:54', '2004-10-19 10:23:54', (SELECT id FROM person WHERE name = 'Sue'));
+--   INSERT INTO kick_session (start_time, end_time, mother_id)
+--    VALUES ('2004-10-19 10:23:54', '2004-10-19 10:23:54', (SELECT id FROM person WHERE name = 'madeline')),
+--    ('2004-10-19 10:23:54', '2004-10-19 10:23:54', (SELECT id FROM person WHERE name = 'Sue')),
+--    ('2004-10-19 10:23:54', '2004-10-19 10:23:54', (SELECT id FROM person WHERE name = 'Sue'));
 
      CREATE TABLE kick (
      id    serial PRIMARY KEY,
      time  TIMESTAMP,
      kick_session_id     	int REFERENCES kick_session (id));
 
-     INSERT INTO kick (time, kick_session_id)
-      VALUES ('2004-10-19 10:23:54', );
+--     INSERT INTO kick (time, kick_session_id)
+--      VALUES ('2004-10-19 10:23:54', );
--- public/kickCounterClient.js
+++ public/kickCounterClient.js
@@ -5,11 +5,115 @@ function toggleCards() {
     login.classList.toggle("d-none");
 }
 
-function doAjaxCall(method, data = null, url, callback) {
+function togglePages(page) {
+    let login = document.getElementById("loginPage");
+    let main = document.getElementById("main");
+    //let kickCounter = document.getElementById("kickCounterPage");
+    switch (String(page)) {
+        case "login":
+            login.classList.remove("d-none");
+            login.classList.add("d-flex");
+            main.classList.add("d-none");
+            break;
+        case "main":
+            main.classList.remove("d-none");
+            login.classList.remove("d-flex");
+            login.classList.add("d-none");
+            break;
+        case "kickCounter":
+            login.classList.add("d-none");
+            login.classList.remove("d-flex");
+            main.classList.remove("d-flex");
+            main.classList.add("d-none");
+            break;
+        default:
+            login.classList.remove("d-none");
+            main.classList.add("d-none");
+            main.classList.remove("d-flex");
+            break;
+    }
+}
 
+function goToMain(json) {
+    // if (this.header.statusCode !== 500) {
+    console.log("back from server with: ");
+    console.log(json);
+    console.log(this.response);
+    const mother = this.response;
+    const name = document.getElementById("motherName");
+    name.innerHTML = mother.username;
+
+
+    //  getKickSessions(mother.id);
+    // }
+    togglePages("main");
+}
+
+function loginMother(event) {
+    event.preventDefault();
+    const form = document.querySelector('#form-login');
+    const formData = serialize(form);
+    doAjaxCall('POST', formData, '/login', true, goToMain);
+}
+
+function signUpMother() {
+    const form = document.querySelector('#form-signin');
+    const formData = serialize(form);
+    doAjaxCall('POST', formData, '/signUp', true, goToMain);
+
+}
+
+function getKickSessions(motherId) {
+    doAjaxCall('GET', null, '/kickSessions/' + motherId, false, callback);
+}
+
+function doAjaxCall(method, data, url, form, callback) {
     let xhr = new XMLHttpRequest();
     xhr.onload = callback;
     xhr.open(method, url, true);
+    console.log(method);
+    console.log(url);
+    if (form) {
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    }
     xhr.responseType = 'json';
-    xhr.send();
+    xhr.send(data);
 }
+
+/*!
+ * Serialize all form data into a query string
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}   form The form to serialize
+ * @return {String}      The serialized form data
+ */
+var serialize = function (form) {
+
+    // Setup our serialized data
+    var serialized = [];
+
+    // Loop through each field in the form
+    for (var i = 0; i < form.elements.length; i++) {
+
+        var field = form.elements[i];
+
+        // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+        if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+
+        // If a multi-select, get all selections
+        if (field.type === 'select-multiple') {
+            for (var n = 0; n < field.options.length; n++) {
+                if (!field.options[n].selected) continue;
+                serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[n].value));
+            }
+        }
+
+        // Convert field data to a query string
+        else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+            serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value));
+        }
+    }
+
+    return serialized.join('&');
+
+};
+
